d88f47e65b33be43e103d4c24186bf53af6984d7
Mike-casper <misha7709123@gmail.com>
2023-07-08 22:26:33 +0300

    Is he gonna survive?

---
+++ 8kyu/solution_9.js
@@ -0,0 +1,14 @@
+
+function hero(bullets, dragons){
+    if(bullets >= dragons * 2){
+        return true
+    } else {
+        return false
+    }
+//Get Coding!
+}
+
+console.log(hero(2, 9))
+console.log(hero(2, 1))
+console.log(hero(9, 3))
+console.log(hero(1, 9))
